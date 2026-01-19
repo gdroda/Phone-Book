3c42732eab5c4e1318c955595ff3af8c6372a9ca
George D. Rodakino <57292409+gdroda@users.noreply.github.com>
2026-01-19 22:14:56 +0200

    Add project files.

---
+++ PhoneBookContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhoneBook
+{
+    public class PhoneBookContext: DbContext
+    {
+        public DbSet<Contact> Contacts { get; set; }
+
+        public string DbPath { get; }
+
+        /*
+        public PhoneBookContext()
+        {
+            // @"Server=(localdb)\\mssqllocaldb;Database=master;Trusted_Connection=True;Integrated security=SSPI);   ConnectRetryCount=0";
+
+        }*/
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=Phonebook;Trusted_Connection=True;Integrated security=SSPI;ConnectRetryCount=0");
+        }
+    }
+
+    public class Contact
+    {
+        public int ContactId { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string EMail { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PhoneBook
+{
+    internal class PhoneBook
+    {
+        static void Main(string[] args)
+        {
+            using var db = new PhoneBookContext();
+
+            Console.Write("Enter name: ");
+            var name = Console.ReadLine();
+            Console.Write("Enter Phone Number: ");
+            var phoneNumber = Console.ReadLine();
+            Console.Write("Enter E-mail: ");
+            var eMail = Console.ReadLine();
+
+            var contact = new Contact { Name = name, PhoneNumber = phoneNumber, EMail = eMail };
+            db.Contacts.Add(contact);
+            db.SaveChanges();
+
+            var query = from c in db.Contacts
+                        orderby c.Name
+                        select c;
+
+            Console.WriteLine("All Contacts");
+            Console.WriteLine("\nName\tPhone Number\tE-mail");
+            foreach (var item in query)
+            {
+                Console.Write($"{item.Name}\t{item.PhoneNumber}\t{item.EMail}\n");
+            }
+
+            Console.WriteLine("\n\nPress ENTER to exit");
+            Console.ReadLine();
+        }
+    }
+}
